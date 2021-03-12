c323d7f7ed7c63e822d68ddce741337d3b0bf6eb
chandler042 <Chandler042@gmail.com>
2021-03-12 11:55:30 -0500

    adding questions in script

---
+++ quiz.js
@@ -0,0 +1,51 @@
+//start with making the questions
+
+const questions = [
+    {
+        question: "What encloses an if/else statement?",
+        answers: [
+            {text: "quotes", correct: false},
+            {text: "regular brackets", correct: false},
+            {text: "curly bracket", correct: true},
+            {text: "greater/less than symbol", correct: false},
+        ],
+    },
+    {
+        question: "What symbols helps distinguish between jquery and javascript?",
+        answers: [
+            {text: "$", correct: true},
+            {text: "!", correct: false},
+            {text: "/", correct: false},
+            {text: "#", correct: false},
+        ],
+    },
+    {
+        question: "How do you add a comment to script?",
+        answers: [
+            {text: "<!--comment-->", correct: false},
+            {text: "// comment", correct: true},
+            {text: "function (comment)", correct: false},
+            {text: "<!--comment-->", correct: false},
+        ],
+    },
+    {
+        question: "Which of the following declares a variable?",
+        answers: [
+            {text: "log", correct: false},
+            {text: "function", correct: false},
+            {text: "var", correct: true},
+            {text: "class", correct: false},
+        ],
+    },
+    {
+        question: "Where would you go to use the debugger command?",
+        answers: [
+            {text: "console", correct: true},
+            {text: "element", correct: false},
+            {text: "network", correct: false},
+            {text: "all of the above", correct: false},
+        ],
+    },
+];
+
+
